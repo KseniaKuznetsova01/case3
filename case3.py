b72a0f387f16ddaa87eca9ea66af29caa832201f
KseniaKuznetsova01 <ksenks.one@gmail.com>
2019-10-22 11:51:31 +0700

    create

--- case3.py
+++ case3.py
@@ -9,9 +9,22 @@ print(lc.DOP)
 dop = int(input())
 
 year = 0
+month = 0
+osnova = start
+kapital = start
+suma = 0
+dop1 = 0
 
-while year <= years:
+while year < years:
     year += 1
     print(year, lc.YEAR)
-    print('-'*46)
-
+    print('-' * 46)
+    while month < 12:
+        month += 1
+        osnova = kapital + dop1
+        suma = osnova * proc / 100
+        kapital = osnova + suma
+        dop1 = dop
+        print('{:3d}\t {:.2f}\t {:.2f}\t {:.2f}'.format(month, osnova, suma, kapital ))
+    if month == 12:
+         month = 0
